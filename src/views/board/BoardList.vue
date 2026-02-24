7f9a65305d2d1ab1f40f9c4a577173f9b64cf44f
dwg245 <dwg0245@gmail.com>
2026-02-24 14:29:08 +0900

    [feat/board/list] 게시판 목록 조회 기능 구현

---
+++ src/views/board/BoardList.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+import api from '@/api/board/index.js'
+
+const list = ref([])
+
+const boardlist = async () => {
+    const boardlist = await api.boardListFun()
+    console.log(boardlist.data)
+    list.value = boardlist.data
+}
+
+</script>
+
+<template>
+    <button @click="boardlist">게시글 조회</button>
+    <div v-for="value in list">
+        <p>{{ value.idx }}</p>
+        <p>{{ value.title }}</p>
+        <p>{{ value.contents }}</p>
+        <hr>
+
+    </div>
+</template>
+
+<style scoped></style>
